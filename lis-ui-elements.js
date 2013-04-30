691b651e4e60fa706ec71ccc3c6ecab863e6b43b
Matiboy <mathieu.chauvinc@gmail.com>
2013-04-30 14:57:33 +0800

    First step for the calendar. Working

--- lis-ui-elements.js
+++ lis-ui-elements.js
@@ -17,28 +17,58 @@
  *
  */
 var module = angular.module('lisUiElements', []);
-module
-  .directive('fdSlider', function ($parse) {
-    return {
-      restrict: 'AE',
-	  replace: false,
-      link: function postLink(scope, element, attrs) {
-		var model = attrs.ngModel;
-		fdSlider.createSlider({
-			inp: element[0],
-			min: attrs.fdSliderMin || 0,
-			max: attrs.fdSliderMax || 100,
-			step: attrs.fdSliderStep || 1,
-			callbacks: {
-			// TODO: extend change callback array instead of replacing
-				change: model ? [function(obj){
-					scope[model] = obj.value;
-					scope.$digest();
-				}] : []
-			}
+	module
+	  .directive('fdSlider', function () {
+		return {
+		  restrict: 'AE',
+		  replace: false,
+		  link: function postLink(scope, element, attrs) {
+			var model = attrs.ngModel;
+			fdSlider.createSlider({
+				inp: element[0],
+				min: attrs.fdSliderMin || 0,
+				max: attrs.fdSliderMax || 100,
+				step: attrs.fdSliderStep || 1,
+				callbacks: {
+				// TODO: extend change callback array instead of replacing
+					change: model ? [function(obj){
+						scope[model] = obj.value;
+						scope.$digest();
+					}] : []
+				}
+			});
+			element.css("display","none");
+		  }
+		};
+	  });
+	
+	function makeKalendae( scope, element, attrs, type ) {
+		attrs.lisOptions = attrs.lisOptions || '{}';
+		var model = attrs.ngModel,
+			options = JSON.parse(attrs.lisOptions),
+		cal = new type(element[0],options);
+		cal.subscribe("change", function(value){
+			// TODO: Range, multiple and multiple calendars
+			scope[model] = value;
+			scope.$digest();
 		});
-		element.css("display","none");
-      }
-    };
-  });
+	}
+	  
+	module.directive('kalendae', function() {
+		return {
+		  restrict: 'EA',
+		  link: function postLink(scope, element, attrs) {
+			makeKalendae(scope, element, attrs, Kalendae);
+		  }
+		};
+	});
+	  
+	module.directive('inputKalendae', function() {
+		return {
+		  restrict: 'A',
+		  link: function postLink(scope, element, attrs) {
+			makeKalendae(scope, element, attrs, Kalendae.Input);
+		  }
+		};
+	});
 })(window, window.angular);
